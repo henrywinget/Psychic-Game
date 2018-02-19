e35da70cb8a9569e945fbbd861c3de9e4f46926d
Henry Winget <henrywinget@yahoo.com>
2018-02-19 16:48:09 -0600

    added logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,56 @@
 var cpuLetter;
 var userLetter;
-var userText = document.getElementById("guess-here");
-var userGuessedLetters = document.getElementById("guessed-letter").innerHTML;
+var userGuessedLetters = [];
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u","v","w","x","y","z"];
+var winsInline = document.getElementById("wins");
+var lossesInLine = document.getElementById("losses");
+var userGuessedIn = document.getElementById("guessed-letter");
+var guessesRemaining = document.getElementById("guesses-remaining");
 
 //Creates a random letter for the CPU
 cpuLetter = letters[Math.floor(Math.random() * letters.length)];
 console.log(cpuLetter);
 
+function updateStats(){
+    winsInline.textContent = wins;
+    lossesInLine.textContent = losses;
+    userGuessedIn.textContent = userGuessedLetters;
+    guessesRemaining.textContent = guesses;
+}
+
 document.onkeyup = function(event){
-//Registers a letter per user key
-userLetter = event.key;
 
-console.log(userLetter);
+    updateStats();
+    //Registers a letter per user key
+    
+    userLetter = event.key
+    userGuessedLetters.push(userLetter);
+    userGuessedIn.textContent = userGuessedLetters + ", ";
 
-//Compares user guess to CPU guess
-for (i = 0; i < letters.length; i++){
-    if (userLetter.toLowerCase === cpuLetter) {
-        document.getElementById("wins").textContent = "Wins: " + 1;
+    if (userLetter === cpuLetter){
+        wins++;
+        guesses = 10;
+        userGuessedIn = [];
+        cpuLetter = letters[Math.floor(Math.random() * letters.length)];;
+    }
 
-    };
+    if (userLetter === userGuessedLetters){
+        guesses = guesses;
+    }
+    else {
+        guesses--;
+
+    }
+    if(guesses ==0){
+        losses++;
+        guesses = 10;
+        userGuessedIn = [];
+        cpuLetter = letters[Math.floor(Math.random() * letters.length)];;
+    }
+    
+    updateStats();
 }
-
-userGuesses = userLetter.innerHTML + ", ";
-
-}
+updateStats();
